d7294daf682858afd330ff9be60a731a3377a3d8
ialirezaesrafili <alirezaesrafili53@gmail.com>
2024-08-05 22:02:08 -0700

    compeleted PLayerInfo

--- src/Player.py
+++ src/Player.py
@@ -1,21 +1,23 @@
 class PlayerInfo:
     """create base class for player and opponents"""
+
     def __init__(self, player_name: str, player_choice: str = None, player_score: int = 0):
         self.player_name = player_name
         self.player_choice = player_choice
         self.player_score = player_score
 
     def get_info(self) -> list:
-        pass
+        return [self.player_name, self.player_choice]
 
     def get_choice(self):
-        pass
+        return self.player_choice
 
     def set_choice(self, choice):
-        pass
+        self.player_choice = choice
 
     def get_score(self):
-        pass
+        return self.player_score
 
     def update_score(self):
-        pass
+        self.player_score += 1
+        return self.player_score
